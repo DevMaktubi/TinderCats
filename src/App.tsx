6da583401d8ba255cedbba517980f72361533064
devmaktubi <brenoluiz1@gmail.com>
2022-01-19 20:58:15 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import { Homepage } from './components/Homepage';
+import { MyCats } from './components/MyCats';
+import { Sidebar } from './components/Sidebar';
+import { Global } from './styles/Global';
 
 function App() {
+  const [favCats, setFavCats] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"flex", overflowX: 'hidden'}}>
+      <Router>
+        <Global />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Homepage favCats={favCats} setFavCats={setFavCats} />} />
+          <Route path="/mycats" element={<MyCats favCats={favCats} setFavCats={setFavCats}/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
